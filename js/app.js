152a1775e2ff503db2a1e9af719c2cfc35acb2a2
abdo hassan <abdoholmes97@gmail.com>
2021-02-01 23:42:40 +0200

    added alert message with no todo added

--- js/app.js
+++ js/app.js
@@ -1,14 +1,29 @@
 const form = document.querySelector('form');
 const todo = document.querySelector('form').todo;
 const listOfTodos = document.querySelector('.list-of-todos');
-
-console.log(listOfTodos);
+const invalidMessage = document.querySelector('.invalid');
 
 // add new todo
 form.addEventListener('submit', addNewTodo);
 function addNewTodo(e) {
   e.preventDefault();
   let todoValue = todo.value;
-  listOfTodos.innerHTML += `<li class='list-group-item'>${todoValue}</li>`;
+  // if submit without value
+  if (todoValue === '') {
+    // show alert message
+    invalidMessage.style.display = 'block';
+  } else {
+    // add new todo to the list
+    listOfTodos.innerHTML += `<li class='list-group-item'>
+    <i class="far fa-circle done" onclick='done()'></i>
+    ${todoValue}
+    </li>`;
+    invalidMessage.style.display = 'none';
+  }
+  // reset form value
   form.reset();
+}
+
+function done() {
+  console.log('done');
 }
