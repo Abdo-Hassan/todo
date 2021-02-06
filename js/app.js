cf7cdef72a863a5c98cd5a4e626375bbb1f7b40c
abdo hassan <abdoholmes97@gmail.com>
2021-02-06 13:43:58 +0200

    added  date to each todo

--- js/app.js
+++ js/app.js
@@ -7,11 +7,12 @@ const listOfDone = document.querySelector('.done-wrapper ul');
 const savedTodos = localStorage.getItem('todos');
 const clearListButton = document.getElementById('clear-list');
 const clearDoneButton = document.getElementById('clear-done');
+const newDate = new Date().toISOString().split('T')[0];
 
 let todos = [
-  { id: 1, todo: 'have breakfast' },
-  { id: 2, todo: 'go to work' },
-  { id: 3, todo: 'sleep well' },
+  { id: 1, todo: 'have breakfast', date: newDate },
+  { id: 2, todo: 'go to work', date: newDate },
+  { id: 3, todo: 'sleep well', date: newDate },
 ];
 
 form.addEventListener('submit', addTodo);
@@ -26,15 +27,16 @@ function addTodo(e) {
     // get last id of todos
     let newId = todos.length ? todos[todos.length - 1].id : 0;
     // add new todo to list of todos
-    let newTodo = { id: ++newId, todo: todoValue };
+    let newTodo = { id: ++newId, todo: todoValue, date: newDate };
     todos.push(newTodo);
     // save to localStorage
     localStorage.setItem('todos', JSON.stringify(todos));
     // add to todos list
     listOfTodos.innerHTML += `<li class='list-group-item'>
-    <i class="far fa-circle done" onclick='doneTodo("${newTodo.todo}-${newTodo.id}")'></i>
-    ${todoValue}
-    <i class="far fa-edit edit" onclick='editTodo("${newTodo.todo}-${newTodo.id}")'></i>
+    <i class="far fa-circle done" onclick='doneTodo("${newTodo.todo}-${newTodo.id}/${newTodo.date}")'></i>
+    <span>${todoValue}</span>
+    <span class='todo-date'>${newTodo.date}</span>
+    <i class="far fa-edit edit" onclick='editTodo("${newTodo.todo}-${newTodo.id}/${newTodo.date}")'></i>
     <i class="far fa-trash-alt remove" onclick='removeTodo("${newId}")'></i>
     </li>`;
     invalidMessage.style.display = 'none';
@@ -46,9 +48,10 @@ function addTodo(e) {
 function showTodos() {
   todos.forEach((todo) => {
     listOfTodos.innerHTML += `<li class='list-group-item'>
-          <i class="far fa-circle done" onclick='doneTodo("${todo.todo}-${todo.id}")'></i>
-          ${todo.todo}
-          <i class="far fa-edit edit" onclick='editTodo("${todo.todo}-${todo.id}")'></i>
+          <i class="far fa-circle done" onclick='doneTodo("${todo.todo}-${todo.id}/${todo.date}")'></i>
+           <span>${todo.todo}</span>
+           <span class='todo-date'>${todo.date}</span>
+          <i class="far fa-edit edit" onclick='editTodo("${todo.todo}-${todo.id}/${todo.date}")'></i>
           <i class="far fa-trash-alt remove" onclick='removeTodo("${todo.id}")'></i>
         </li>`;
   });
@@ -60,6 +63,11 @@ function doneTodo(todoInfo) {
   let idTodo = todoInfo.split('-')[1];
   // -todo value
   let valueTodo = todoInfo.split('-')[0];
+  // -todo Date
+  let toodDate = todoInfo.split('/')[1];
+  console.log('idTodo', idTodo);
+  console.log('valueTodo', valueTodo);
+  console.log('toodDate', toodDate);
   // save to localStorage
   // localStorage.setItem('done', JSON.stringify(todos));
   // get todo id index
@@ -71,11 +79,18 @@ function doneTodo(todoInfo) {
   todos.splice(idIndex, 1);
 
   //remove todo from original list
-  listOfTodos.removeChild(listOfTodos.childNodes[idIndex]);
+  // listOfTodos.removeChild(listOfTodos.childNodes[idIndex]);
+  console.log(idIndex);
+  console.log(listOfTodos.childNodes);
   //show done todos section
   doneWrapper.style.display = 'block';
   // add done todos to done todos list
-  listOfDone.innerHTML += `<li class='list-group-item'>${valueTodo}</li>`;
+  listOfDone.innerHTML += `
+  <li class='list-group-item'>
+  <span>${valueTodo}</span>
+  <span class='todo-date-done'>${toodDate}</span>
+  </li>
+  `;
 }
 
 // edit todo
